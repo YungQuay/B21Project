45d82fd6457d7438c81743dea40a14947200f390
Ruslan Petrov <richhomiequan94@mail.ru>
2020-11-10 12:49:18 -0500

    project is already in remote repo

--- src/HAHA.java
+++
@@ -1,5 +0,0 @@
-public class HAHA {
-    public static void main(String[] args) {
-        System.out.println("123");
-    }
-}
--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class Hello {
+    // more stuff
+    // Mac user....
+
+    //I have already published in Github.com
+    //now I am ready to make more commits
+    //etc
 }
+
